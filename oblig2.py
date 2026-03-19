dfdd3d49dfe8b015af06a506d0611ca663ac0974
Akiranravi <akrav9842@oslomet.no>
2026-03-19 19:26:18 +0100

    Legger til likningen g(x) fra utregningen

--- oblig2.py
+++ oblig2.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 # Definer f(x)
 def f(x):
     return np.exp(-x/4) * np.arctan(x)
+
+# Likninga fra den analytiske utregningen
+# arctan(x) - 4/x^2+1 = 0
+def g(x):
+    return np.arctan(x) - 4/(x**2 + 1)
