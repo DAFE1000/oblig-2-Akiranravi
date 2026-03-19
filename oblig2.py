f518dcb936ba1316ad56b2a26ecd7acc0d86c1f4
Akiranravi <akrav9842@oslomet.no>
2026-03-19 19:35:31 +0100

    Finn toppunkt numerisk

--- oblig2.py
+++ oblig2.py
@@ -9,3 +9,14 @@ def f(x):
 # arctan(x) - 4/x^2+1 = 0
 def g(x):
     return np.arctan(x) - 4/(x**2 + 1)
+
+# Prøver 100 000 x-verdier og finner der g(x) er nærmest 0
+x_verdier = np.linspace(0, 5,100000)
+g_verdier = g(x_verdier)
+
+idx = np.argmin(np.abs(g_verdier)) # finner indeksen til den minste verdien
+x_topp = x_verdier[idx]
+y_topp = f(x_topp)
+
+print(f"Toppunkt x = {x_topp:.4f}")
+print(f"Toppunkt y = {y_topp:.4f}")
